63a52f3cddf7bd73c4400fa8e5e2b8809f58c4f8
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-03-14 16:25:04 +0300

    Added screenshot for README

--- app/src/main/java/ru/bonsystems/tothevoid/assets/utils/Pallete.java
+++ app/src/main/java/ru/bonsystems/tothevoid/assets/utils/Pallete.java
@@ -9,6 +9,6 @@ public class Pallete {
     public final static int SPACE_LIGHT = Color.argb(255, 83, 76, 90);
     public final static int SPACE_DARK = Color.argb(255, 52, 46, 61);
     public final static int ACCENT_LIGHT = Color.argb(255, 239, 236, 213);
-    public final static int BULLETS_COLOR = 0xfff1c101;
+    public final static int BULLETS_COLOR = Color.parseColor("#f1c101");
     public final static int BULLETS_AREAL_COLOR = Color.argb(255, 255, 128, 52);
 }
